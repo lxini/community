8148e01914a3bbb1ff4b2590bdfccaf7027144ca
李鑫 <li.xin@umedcloud.com>
2020-05-13 14:16:51 +0800

    add flyway migration support

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `account_id` varchar(100),
+  `name` varchar(50),
+  `token` char(36),
+  `gmt_create` bigint,
+  `gmt_modified` bigint,
+);
