60dda528d04f2ec300e25e10d926278e0c61c4cc
李鑫 <li.xin@umedcloud.com>
2020-05-13 13:37:56 +0800

    添加cookies检查

--- src/main/java/life/majiang/community/contorller/IndexController.java
+++ src/main/java/life/majiang/community/contorller/IndexController.java
@@ -1,13 +1,33 @@
 package life.majiang.community.contorller;
 
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Resource
+    UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }
